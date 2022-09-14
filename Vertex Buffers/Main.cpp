5476c517dfe3f43b18343fb3ec34ad4a8cb41ac3
TurboKox <93782955+TurboKox@users.noreply.github.com>
2022-09-14 14:44:07 +0200

    Added vertex array

--- Vertex Buffers/Main.cpp
+++ Vertex Buffers/Main.cpp
@@ -119,6 +119,10 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     window = glfwCreateWindow(600, 600, "Index Buffers", NULL, NULL);
     if (!window)
     {
@@ -133,6 +137,8 @@ int main(void)
     if (glewInit() != GLEW_OK)
         std::cout << "Error!" << std::endl;
 
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
     float positions[] = {
         -0.5f, -0.5f, // 0
          0.5f, -0.5f, // 1
@@ -145,18 +151,22 @@ int main(void)
         2, 3, 0
     };
 
+    unsigned int vao; // vertex array object
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao));
+
     unsigned int buffer;
     GLCall(glGenBuffers(1,&buffer));
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-    GLCall(glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
     
     GLCall(glEnableVertexAttribArray(0));
-    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*)0));
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
     // with pointer value of 0 (last argument) it's not necessary to convert it to (const void*)
-    // however with any other value for example 8 is inevitable
+    // however with any other value (for example 8) it's inevitable
 
     unsigned int ibo; // index buffer object
-    GLCall(glGenBuffers(1, &ibo));
+    GLCall(glGenBuffers(1, &ibo)); 
     GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
     GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
@@ -169,6 +179,11 @@ int main(void)
     ASSERT(location != -1);
     GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
 
+    GLCall(glBindVertexArray(0));
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
     float r = 0.0f;
     float increment = 0.03f;
 
@@ -176,7 +191,11 @@ int main(void)
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
+        GLCall(glUseProgram(shader));
         GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+        GLCall(glBindVertexArray(vao));
+
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
         if (r > 1.0f)
